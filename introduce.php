9f3f882e204b00b134c77a4c12c57727538c24fa
armjubjub <32847689+armjubjub@users.noreply.github.com>
2019-01-17 00:23:31 +0700

    add file introduce.php
    add file

---
+++ introduce.php
@@ -0,0 +1,34 @@
+<html>
+<head ><link rel="stylesheet" type="text/css" href="dc.css"></head>
+<body background = bgt.jpg>
+<table  >
+<th width=80> 
+<a href = home.php><img src = home.png width=100%></a>
+</th>
+<th width=100>
+<div class="dropdown">
+  <button class="dropbtn">ตัวเลือก</button>
+  <div class="dropdown-content">
+    <a href="profile.php">ประวัติส่วนตัว</a>
+    <a href="education.php">ประวัติการศึกษา</a>
+    <a href="introduce">ข้อมูลทั่วไป</a>
+  </div>
+</div>
+</th>
+</table >
+<center><img src = in.png><br>
+<img src = แมว.png width = 20%>
+<?php
+echo "<table bgcolor=#66CC66>
+<tr><td>สัตว์เลี้ยงที่ชอบ</td><td>แมว</td></tr>
+<tr><td>สีที่ชอบ</td><td>สีชมพู</td></tr>
+<tr><td>อาหารที่ชอบ</td><td>ข้าวผัด</td></tr>
+<tr><td>งานอดิเรก</td><td>นอน</td></tr>
+<tr><td>สถานที่ท่องเที่ยวที่อยากไป</td><td>สวนสนุก</td></tr>
+<tr><td>ความใฝ่ฝันในอนาคต</td><td>อยากมีฟาร์มแมว</td></tr>
+<tr><td>คติประจำใจ</td><td>ทำให้เต็มที่กับสิ่งที่เลือก</td></tr>
+</table> "
+?>
+</center>
+</body>
+</html>
